5f57183410260924f05f0af8f81f1ed89ed38090
brenoaguirres <142112771+brenoaguirres@users.noreply.github.com>
2024-02-02 14:32:00 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+    data() {
+        // variables
+        return {
+
+        }
+    },
+    methods: {
+        printHello() {
+            console.log("Hello World!")
+        }
+    },
+    computed: {
+        //filtered and computed variables
+    }
+})
+
+app.mount("#app")
